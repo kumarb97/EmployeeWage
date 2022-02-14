70e27f06d399ae37e5f62677021dfb43a60ef016
kumarb97 <kumarbamankar@gmail.com>
2022-02-14 19:07:01 +0530

    emp_parttime added

---
+++ employeewage/emp_parttime.java
@@ -0,0 +1,24 @@
+package employeewage;
+
+public class emp_parttime {
+	public static void main(String[] args) {
+		int wage_per_hr = 20;
+		int emp_worked_hr = 8;
+		int parttime_worked = 8;
+		double emp_check =Math.floor((Math.random() * 10) % 2);
+		if (emp_check == 1)
+		{
+			System.out.println("Employee is present and");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+		if (emp_check == 1)
+		{
+			int emp_daily_wage = (wage_per_hr * emp_worked_hr) + (parttime_worked * wage_per_hr);
+			System.out.println("Employee's daily wage is " +emp_daily_wage);
+		}
+
+}
+}
